386a7ea7530fac682bf5e97f3f0f32e189093a60
Meaad Hassan <meaadhassan09@gmail.com>
2020-10-28 14:56:55 -0400

    PhP update

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -4,24 +4,17 @@
     $subject = $_Post['subject'];
     $message = $_Post['message'];
 
-    $email_from= 'Starpharmacy@Starpharmacy.com';
+    $to= 'starpharmacymi@gmail.com';
 
     $email_subject= "New Website Form Submission";
 
-    $email_body= "User Name: $name.\n"
-                    "User Email: $visitor_email.\n".
-                        "User Subject: $subject.\n".
-                            "User Message: $message.\n";
+    $email_body= "";
 
-    #to= "meaadhassan09@gmail.com";
-
-    $headers ="From: $email_from \r \n";
-
-    $headers .="Reply-To: $visitor_email \r\n";
-    
-    mail($to, $email_subject, $email_body,$headers);
-
-    header("Location: index.html");
-
+    $email_body.= "User Name: ".$name."\r\n";
+    $email_body.= "User Email:" .$visitor_email."\r\n";
+    $email_body.= "User Subject:" .$subject."\r\n";
+    $email_body.= "User Message:" .$message."\r\n";
+   
+    mail($to, $email_subject, $email_body);
     
 ?>
