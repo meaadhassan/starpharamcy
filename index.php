b084ec8986b242daf18749e65c5d7aad3d293f50
Meaad Hassan <meaadhassan09@gmail.com>
2020-10-28 14:32:13 -0400

    update website

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?
-$ echo '{}' > composer.json
-$ git add composer.json
-$ git commit -m "add composer.json for PHP app detection"
-$ heroku buildpacks:set https://github.com/heroku/heroku-buildpack-php
     php header( 'Location: /index.html' ) ;  
 ?>
