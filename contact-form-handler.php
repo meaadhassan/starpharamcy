4c4a905ddc498edc204b45087105e5c983137ad8
Meaad Hassan <meaadhassan09@gmail.com>
2020-10-28 20:07:40 -0400

    php update

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -4,17 +4,13 @@
     $subject = $_Post['subject'];
     $message = $_Post['message'];
 
-    $to= 'meaadhassan09@gmail.com';
+    $to= 'meaad_hassan@yahoo.com';
     $headers= "From: ". $visitor_email;
 
     $email_subject= "New Website Form Submission about ";
 
-    $email_body= "";
 
-    $email_body.= "User Name: ".$name."\r\n";
-    $email_body.= "User Email:" .$visitor_email."\r\n";
-    $email_body.= "User Subject:" .$subject."\r\n";
-    $email_body.= "User Message:" .$message."\r\n";
+    $email_body= "User Name: ".$name."\r\n User Email:" .$visitor_email."\r\n User Subject:" .$subject."\r\n User Message:" .$message."\r\n";
    
     mail($to, $email_subject, $email_body);
     header( 'Location: /index.html' ) ;  
